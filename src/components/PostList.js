75acd38bc4041cd6066ffd4b0377759b1edb9a66
Philip <pch4623@gmail.com>
2022-02-06 14:37:30 +0900

    Action creator 연동

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
-const PostList = () => {
-  
-  return (
-    <div>PostList!</div>
-  );
-};
+class PostList extends Component {
 
-export default PostList;
+  // 렌더링 후 Post 데이터 fetch 하는 Action Creator 호출
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>PostList!</div>
+    );
+  }
+}
+
+export default connect(
+  null,           // 현재 Redux store에서 관리할 state 가 없는 상태
+  { fetchPosts }  // Action Creators (key-value 같은 경우 생략 가능 : ES6)
+)(PostList);
