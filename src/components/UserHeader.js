970264e5bae51fc9b4178cc87bf4f8e981c5db69
Philip <pch4623@gmail.com>
2022-02-12 00:12:55 +0900

    재사용성 향상을 위해 mapStateToProps 안에서 pre-calculation 수행

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -9,7 +9,8 @@ class UserHeader extends Component {
   }
 
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId);
+    //const user = this.props.users.find(user => user.id === this.props.userId);  -> mapStateToProps에서 수행
+    const { user } = this.props;
     
     if (!user)
       return null;
@@ -18,8 +19,9 @@ class UserHeader extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return { users : state.users };
+const mapStateToProps = (state, ownProps) => {  // ownProps는 PostList에서 UserHeader 컴포넌트 호출 시 넘긴 props를 참조
+  // 재사용성 향상을 위해 해당하는 user 만 렌더링 하도록 pre-calculation
+  return { user : state.users.find(user => user.id === ownProps.userId) };
 }
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
